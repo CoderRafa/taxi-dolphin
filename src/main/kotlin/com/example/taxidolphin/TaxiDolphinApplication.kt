22ad23a08edc05819256636f31611c2e9ad825de
Rafayel Khachatryan <rafar2005@yandex.ru>
2024-10-17 19:13:14 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/taxidolphin/TaxiDolphinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.taxidolphin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaxiDolphinApplication
+
+fun main(args: Array<String>) {
+    runApplication<TaxiDolphinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/taxidolphin/TaxiDolphinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.taxidolphin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaxiDolphinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
