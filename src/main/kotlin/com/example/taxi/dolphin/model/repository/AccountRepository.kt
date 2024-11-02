03bd9603bbe1bcdb6eea857aeeaa10e5ed909eb0
Rafayel Khachatryan <rafar2005@yandex.ru>
2024-11-02 20:18:43 +0200

    Tried to create database

--- src/main/kotlin/com/example/taxi/dolphin/model/repository/AccountRepository.kt
+++ src/main/kotlin/com/example/taxi/dolphin/model/repository/AccountRepository.kt
@@ -1,8 +1,8 @@
 package com.example.taxi.dolphin.model.repository
 
-import com.example.taxi.dolphin.model.entity.UserEntity
+import com.example.taxi.dolphin.model.entity.AccountEntity
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface AccountRepository: CrudRepository<AccountRepository, Long>
+interface AccountRepository: CrudRepository<AccountEntity, Long>
