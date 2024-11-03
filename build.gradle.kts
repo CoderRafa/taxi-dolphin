223df838767fb4919eef2675bbff83d1e52b3f16
k.iakimchuk <k.yakimchuk@ventra.ru>
2024-11-03 10:49:15 +0300

    Add more properties.

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,11 +32,11 @@ dependencies {
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-    runtimeOnly("com.h2database:h2")
     runtimeOnly("org.postgresql:postgresql")
 
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
+    testRuntimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- src/main/kotlin/com/example/taxi/Application.kt
+++ src/main/kotlin/com/example/taxi/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class TaxiDolphinApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<TaxiDolphinApplication>(*args)
+    runApplication<Application>(*args)
 }
